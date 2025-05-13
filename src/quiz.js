663d6dc391f2d6715f120077ce38519a3ab226ff
Alec E <alexcmenc@gmail.com>
2025-05-13 23:40:01 +0100

    project done

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,57 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.score = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 4. shuffleQuestions()
+  moveToNextQuestion () {
+    return (this.currentQuestionIndex = this.currentQuestionIndex + 1);
+  }
 
-    // 5. checkAnswer(answer)
+  shuffleQuestions () {
+    return this.questions.sort(() => Math.random() - 0.5);
+  }
 
-    // 6. hasEnded()
+  checkAnswer (answer) {
+    const question = this.getQuestion();
+    const isCorrect = question.answer === answer;
+
+    if (isCorrect) {
+      this.score += 1;
+    }
+    return isCorrect;
+  }
+
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    }
+    if (this.currentQuestionIndex === this.questions.length) {
+      return true;
+    }
+  }
+
+  filterQuestionsByDifficulty(difficulty) {
+    if (difficulty >= 1 && difficulty <= 3) {
+      this.questions = this.questions.filter(
+        (question) => question.difficulty === difficulty
+      );
+    }
+  }
+
+  averageDifficulty() {
+    if (this.questions.length === 0) return 0;
+    const total = this.questions.reduce(
+      (acc, question) => acc + question.difficulty,
+      0
+    );
+    return total / this.questions.length;
+  }
 }
